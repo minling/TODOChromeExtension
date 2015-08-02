22d5e7e3b6165ad4bbc897762878426fc084ce5d
minling <mzhao725@gmail.com>
2015-08-02 01:05:31 -0400

    added current time and css for it

--- datetime.js
+++ datetime.js
@@ -1,11 +1,30 @@
-var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+function date(){
+  var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
-var date = new Date();
-var month = date.getMonth();
-var day = date.getDate();
-var dayofweek = date.getDay();
-var year = date.getFullYear();
-var dateToday = "Today is " +days[day] + ", "+ months[month] + " "+ day +", "+ year;
+  var date = new Date();
+  var month = date.getMonth();
+  var day = date.getDate();
+  var dayofweek = date.getDay();
+  var year = date.getFullYear();
+  var dateToday = "Today is " +days[day] + ", "+ months[month] + " "+ day +", "+ year;
+  return dateToday;
+}
 
-document.getElementById("datetime").innerHTML = dateToday;
+document.getElementById("datetime").innerHTML = date();
+
+function time(){
+  var date = new Date();
+  var hour = date.getHours();
+  var minutes = date.getUTCMinutes();
+  var ampm = hour < 12 ? "AM" : "PM";
+  var currentHour = hour % 12;
+  currentHour = currentHour? hour : 12;
+  minutes = minutes? "0"+minutes : minutes;
+
+  var timeNow = currentHour + ":"+ minutes + " "+ ampm;
+  return timeNow
+}
+
+document.getElementById("time").innerHTML = time();
+
